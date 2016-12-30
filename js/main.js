af4c87757b30cdadac24048a07a1a511c91331f9
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-12-29 22:06:55 -0500

    Adds toolbar, toolbox, and can now draw floor tiles.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,65 @@
 var WIDTH = 14;
 var HEIGHT = 10;
-var UNIT = 48;
+var UNIT = 16;
+var MULT = 3;
 
 function Builder() {
-  this.buildCanvas();
+  this.width = WIDTH * UNIT * MULT;
+  this.height = HEIGHT * UNIT * MULT;
 
+  this.buildCanvas();
   this.buildCells();
 
   var builder = this;
-  $('.cells').click(function(e) {
+  $('.cell').mousedown(function(e) {
     var x = $(e.target).attr('data-x');
     var y = $(e.target).attr('data-y');
-    builder.cellClick(x, y);
-  })
+    builder.cellMouseDown(x, y);
+  });
+
+  var builder = this;
+  $('.cell').mouseup(function(e) {
+    var x = $(e.target).attr('data-x');
+    var y = $(e.target).attr('data-y');
+    builder.cellMouseUp(x, y);
+  });
+  $('.cell').mouseenter(function(e) {
+    var x = $(e.target).attr('data-x');
+    var y = $(e.target).attr('data-y');
+    builder.cellMouseEnter(x, y);
+  });
+  $('.actions__button').click(function(e) {
+    var type = $(e.target).attr('data-type');
+    builder.actionButtonClick(type);
+  });
+
+  this.mouseDown = false;
+  this.selectedImage = 'img/flooring/wood-light.png';
 }
 
 
 
 Builder.prototype.buildCanvas = function() {
   var canvasContainer = $('.canvas');
-  this.canvas = document.createElement('canvas');
-  this.context = this.canvas.getContext("2d"); // Context of the canvas.
-  this.canvas.width = WIDTH * UNIT;
-  this.canvas.height = HEIGHT * UNIT;
-  $(canvasContainer).append(this.canvas);
+  this.displayCanvas = document.createElement('canvas');
+  this.displayContext = this.displayCanvas.getContext('2d'); // Context of the canvas.
+  this.displayCanvas.width = this.width;
+  this.displayCanvas.height = this.height;
+
+  this.manipCanvas = document.createElement('canvas');
+  $(this.manipCanvas).addClass('hide');
+  this.manipContext = this.manipCanvas.getContext('2d'); // Context of the canvas.
+
+  $(canvasContainer).append(this.displayCanvas);
+  $(canvasContainer).append(this.manipCanvas);
 };
 
 
 
 Builder.prototype.buildCells = function() {
   var cellsContainer = $('.cells');
-  for (var x = 0; x < WIDTH; x++) {
-    for (var y = 0; y < HEIGHT; y++) {
+  for (var y = 0; y < HEIGHT; y++) {
+    for (var x = 0; x < WIDTH; x++) {
       var div = document.createElement('div');
       $(div).addClass('cell')
             .attr('data-x', x)
@@ -44,8 +72,75 @@ Builder.prototype.buildCells = function() {
 
 
 
-Builder.prototype.cellClick = function(x, y) {
-  console.log(x, y);
+Builder.prototype.fillCell = function(x, y) {
+  // Create a new image, set its onload function, and set the source
+  var image = new Image();
+  var pool = this;
+  image.onload = function() {
+    pool.drawImageOnLoad(this, x, y);
+  }
+  image.src = this.selectedImage;
+};
+
+
+
+
+Builder.prototype.cellMouseDown = function(x, y) {
+  this.mouseDown = true;
+  this.fillCell(x, y);
+};
+
+
+
+
+Builder.prototype.cellMouseUp = function(x, y) {
+  this.mouseDown = false;
+};
+
+
+
+
+Builder.prototype.cellMouseEnter = function(x, y) {
+  if (this.mouseDown) this.fillCell(x, y);
+};
+
+
+
+
+Builder.prototype.actionButtonClick = function(type) {
+  switch(type) {
+    case 'toggle-grid':
+      $('.canvas').toggleClass('gridded');
+      break;
+
+    case 'clear-all':
+      var shouldClear = confirm('Do you want to erase your world?');
+      if (shouldClear) this.displayContext.clearRect(0, 0, this.displayCanvas.width, this.displayCanvas.height);
+      break;
+
+    default:
+      break;
+  }
+};
+
+
+
+// Called when the image to draw has been loaded.
+Builder.prototype.drawImageOnLoad = function(image, x, y) {
+  // Set the manipulation canvas to the image height and width
+  // This canvas will always be the size of the original image
+  this.manipCanvas.width = image.width * MULT;
+  this.manipCanvas.height = image.height * MULT;
+
+  // Draw original image on manipulation canvas
+  // Turn off image smoothing so pixels render exactly
+  this.manipContext.imageSmoothingEnabled = false;
+  this.manipContext.scale(MULT, MULT);
+  this.manipContext.drawImage(image, 0, 0);
+
+  // Scale the display canvas & draw the manipulation canvas content
+  this.displayContext.imageSmoothingEnabled = false;
+  this.displayContext.drawImage(this.manipCanvas, x * UNIT * MULT, y * UNIT * MULT);
 };
 
 
